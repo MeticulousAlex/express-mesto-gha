54c1eb40b28a910e72bed85f2bd5699eccfae903
Aleksandr Smelov <smelovalex14@gmail.com>
2023-11-01 13:22:20 +0200

    ref: avatar-link validation

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().dataUri(),
+    avatar: Joi.string().uri({
+      scheme: [
+        'http',
+        'https',
+      ],
+    }),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),

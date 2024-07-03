2e5cadaaf62a39907c9183e3cb2a429a812ab3ca
Aleksandr Smelov <smelovalex14@gmail.com>
2024-07-03 13:34:11 +0300

    auth changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,18 +5,17 @@ const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const jwtToken = req.cookies.jwt.replace('jwt=', '');
-  console.log(jwtToken);
 
   let payload;
 
   try {
     payload = jwt.verify(jwtToken, JWT_SECRET);
     if (!payload) {
-      throw new UnauthorizedError('Необходима авторизация');
+      throw new UnauthorizedError(`${jwtToken}`);
     }
   } catch (err) {
     if (err.name === 'JsonWebTokenError') {
-      throw new UnauthorizedError('Необходима авторизация');
+      throw new UnauthorizedError('плохой токен'); // Необходима авторизация
     }
     next(err);
   }
